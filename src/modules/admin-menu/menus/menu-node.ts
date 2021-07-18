320d49bd0d03cb789c1d5511c990ecacdbf306a1
Maksym Grom <MailMaximGrom@gmail.com>
2021-07-18 18:35:08 +0300

    Добавлены тесты для Menu API сервиса

--- src/modules/admin-menu/menus/menu-node.ts
+++ src/modules/admin-menu/menus/menu-node.ts
@@ -7,6 +7,7 @@ export interface MenuNode {
   id: string;
   parentId: string;
   sortOrder: number;
+  removed?: boolean;
 }
 
 export type PatchMenuNode = Pick<MenuNode, 'id'>
--- src/modules/admin-menu/menus/menu.service.spec.ts
+++ src/modules/admin-menu/menus/menu.service.spec.ts
@@ -33,5 +33,191 @@ describe('MenuService Unit Test', () => {
       });
       expect(menuService.getMenu()).toHaveLength(1);
     });
+
+    it('Листовой узел без ссылки должен быть удален', () => {
+      menuService.add({
+        id: 'foo',
+        parentId: ROOT_MENU_NODE_ID,
+        sortOrder: 10,
+        name: 'foo',
+      });
+      expect(menuService.getMenu()).toHaveLength(0);
+    });
+
+    it('Узлы с одинаковым ID перетирают друг дурга', () => {
+      menuService.add({
+        id: 'foo',
+        parentId: ROOT_MENU_NODE_ID,
+        sortOrder: 10,
+        name: 'foo',
+        href: 'https://foo.com'
+      });
+
+      menuService.add({
+        id: 'foo',
+        parentId: ROOT_MENU_NODE_ID,
+        sortOrder: 20,
+        name: 'foo',
+        href: 'https://foo.com'
+      });
+      const menu = menuService.getMenu();
+      expect(menu).toHaveLength(1);
+      expect(menu[0].sortOrder).toBe(20);
+    });
+
+    it('Узлы выстраиваются в иерархию', () => {
+      menuService.add({
+        id: 'bar',
+        parentId: 'foo',
+        sortOrder: 10,
+        name: 'bar',
+        href: 'https://bar.com'
+      });
+
+      menuService.add({
+        id: 'foo',
+        parentId: ROOT_MENU_NODE_ID,
+        sortOrder: 20,
+        name: 'foo',
+        href: 'https://foo.com'
+      });
+      const menu = menuService.getMenu();
+      expect(menu).toHaveLength(1);
+      expect(menu[0].id).toBe('foo');
+      expect(menu[0].children).toHaveLength(1);
+      expect(menu[0].children[0].id).toBe('bar');
+    });
+
+    it('Вложенные узлы добавляются с привязкой к родителю', () => {
+      menuService.add({
+        id: 'foo',
+        parentId: ROOT_MENU_NODE_ID,
+        sortOrder: 20,
+        name: 'foo',
+        href: 'https://foo.com',
+        children: [
+          {
+            id: 'bar',
+            parentId: ROOT_MENU_NODE_ID,
+            sortOrder: 10,
+            name: 'bar',
+            href: 'https://bar.com'
+          }
+        ]
+      });
+
+      const menu = menuService.getMenu();
+      expect(menu).toHaveLength(1);
+      expect(menu[0].id).toBe('foo');
+      expect(menu[0].children).toHaveLength(1);
+      expect(menu[0].children[0].id).toBe('bar');
+    });
+  });
+
+  describe('Patch node', () => {
+    it('Добавленный узел можно переписать' +
+      ' независимо от очередности вызова', () => {
+      menuService.patch({
+        id: 'foo',
+        sortOrder: 77
+      });
+      menuService.add({
+        id: 'foo',
+        parentId: ROOT_MENU_NODE_ID,
+        sortOrder: 20,
+        name: 'foo',
+        href: 'https://foo.com',
+      });
+
+      const menu = menuService.getMenu();
+      expect(menu[0].sortOrder).toBe(77);
+      expect(menu[0].name).toBe('foo');
+    });
+
+    it('Patch не добавляет узел, а лишь модифицирует', () => {
+      menuService.patch({
+        id: 'foo',
+        sortOrder: 77
+      });
+
+      const menu = menuService.getMenu();
+      expect(menu).toHaveLength(0);
+    });
+
+    it('Можно иметь более 1 патча', () => {
+      menuService.patch({
+        id: 'foo',
+        sortOrder: 77,
+        href: 'https://bar.com'
+      });
+      menuService.add({
+        id: 'foo',
+        parentId: ROOT_MENU_NODE_ID,
+        sortOrder: 20,
+        name: 'foo',
+        href: 'https://foo.com',
+      });
+      menuService.patch({
+        id: 'foo',
+        sortOrder: 13,
+        name: 'bar'
+      });
+
+      const menu = menuService.getMenu();
+      expect(menu[0].sortOrder).toBe(13);
+      expect(menu[0].href).toBe('https://bar.com');
+      expect(menu[0].name).toBe('bar');
+    });
+  });
+
+  describe('Remove node', () => {
+    it('Узел может быть удален по его ID', () => {
+      menuService.remove('foo', 'bar');
+      menuService.add({
+        id: 'foo',
+        parentId: ROOT_MENU_NODE_ID,
+        sortOrder: 20,
+        name: 'foo',
+        href: 'https://foo.com',
+      });
+
+      expect(menuService.getMenu()).toHaveLength(0);
+    });
+
+    it('Patch может отменить удаление', () => {
+      menuService.add({
+        id: 'foo',
+        parentId: ROOT_MENU_NODE_ID,
+        sortOrder: 20,
+        name: 'foo',
+        href: 'https://foo.com',
+      });
+      menuService.remove('foo', 'bar');
+      menuService.patch({
+        id: 'foo', removed: false
+      }, {
+        id: 'bar', removed: false
+      });
+
+      expect(menuService.getMenu()).toHaveLength(1);
+    });
+
+    it('Patch === remove, зависят от очереди вызова', () => {
+      menuService.add({
+        id: 'foo',
+        parentId: ROOT_MENU_NODE_ID,
+        sortOrder: 20,
+        name: 'foo',
+        href: 'https://foo.com',
+      });
+      menuService.patch({
+        id: 'foo', removed: false
+      }, {
+        id: 'bar', removed: false
+      });
+      menuService.remove('foo', 'bar');
+
+      expect(menuService.getMenu()).toHaveLength(0);
+    });
   });
 });
